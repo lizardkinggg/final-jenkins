ad8e743f21292b7347ad4cfe0eae4a447769933e
Jenkins <jenkins@container-jenkins.us-central1-a.c.learn-gcp-267115.internal>
2020-03-03 05:07:28 +0000

    already

--- testlinter.sh
+++ testlinter.sh
@@ -4,7 +4,7 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
 export NVM_DIR="/var/lib/jenkins/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-source /var/lib/jenkins/getsentry/sentry/fooo/bin/activate
+source /var/lib/jenkins/foo/bin/activate
 
 echo "added variables"
 
@@ -14,7 +14,7 @@ export PERCY_PARALLEL_TOTAL=1
 export PERCY_TOKEN=f7ae116d11ac0b11466f46b61be7b56177931f14abcc6feff081fea0a50ccc13
 export STORYBOOK_PERCY_TOKEN=f473fdfe11f1a2d03ffb28e73a9d322ce46ddd12700ab0ec7720482b09c66e98
 export ZEUS_HOOK_BASE=https://zeus.ci/hooks/fa079cf6-8e6b-11e7-9155-0a580a28081c/28a09c85e75d6d74f7697786dd208b4f91ec51c9347f5e35f07a8327dfcc353b
-export CI_COMMIT_SHA="47a7b0d9dd0f5a47c4fa44ed80849ce5cc1f5f82"
+#export CI_COMMIT_SHA="47a7b0d9dd0f5a47c4fa44ed80849ce5cc1f5f82"
 export NODE_ENV="development"
 export PIP_DISABLE_PIP_VERSION_CHECK="on"
 export PIP_QUIET=1
